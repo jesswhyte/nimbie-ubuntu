a0cf385cab5f9a1c023dc51acd1b7d23c14808bc
Jess Whyte <jess.whyte@gmail.com>
2023-02-01 16:40:42 -0500

    updated tray eject commands, added a warmup, cleaned up the status updates

--- nimbie.sh
+++ nimbie.sh
@@ -34,8 +34,16 @@ gsettings set org.gnome.desktop.media-handling automount-open false
 
 autoloader="sudo /usr/local/bin/autoloader" # path to executable on local machine - not needed if added to path, etc.
 
+## sometimes, the cd tray doesn't want to eject the first few times, but a warmup helps. TODO: look into this, but this works for now...
+echo && echo "warming up CD tray..." && echo
+eject /dev/sr1
+eject -t /dev/sr1
+eject /dev/sr1
+eject -t /dev/sr1
+echo "warmup done..." && echo 
+
 cdcheck(){
-	# this function adapted from cd.close - by https://superuser.com/users/464868/allan 
+	# this function adapted from cd.close - written by https://superuser.com/users/464868/allan 
 	CDROM=/dev/sr1
 	TRIES="1 2 3 4"
 	INTERVAL=5
@@ -69,21 +77,24 @@ cdcheck(){
 		#printf "final: LABEL "
 		volname $CDROM
 	else
-		echo final: NO MEDIUM DETECTED
+		echo "final: NO MEDIA DETECTED"
 		reject
 		break
 	fi
 }
 
 reject(){
-	echo REJECTING... 
+	echo "REJECTING CD...will eject underneath the Nimbie..." && echo 
 	eject /dev/sr1 && $autoloader PICK && eject -t /dev/sr1 && $autoloader REJECT
 }
 
+## initialize autoloader
+echo "Intizializing Autoloader..." && echo
 $autoloader INIT ## initialize the nimbie
+echo 
 
 s_status="+S07" ## set starting s_status
- 
+
 while [ "s_status" != "+S14" ]; do
 
 	eject /dev/sr1 ## eject the drive tray
@@ -93,7 +104,8 @@ while [ "s_status" != "+S14" ]; do
 	s_status=`echo $output | grep -oP "\+S\d\d"` # get the S-code from the $autoloader response, $autoloader S14 = no more disks
 	
 	if [[ $s_status == "+S14" ]]; then
-	  echo "Done all the disks!"
+	  echo && echo "Done all the disks!" && echo 
+	  eject -t /dev/sr1 
 	  exit 0
 	fi
 	
@@ -102,19 +114,19 @@ while [ "s_status" != "+S14" ]; do
 	cdcheck ## check cd is loaded
 
 	#cdinfo=`isoinfo -d -i /dev/sr1` # get isoinfo
+
+	time=`date +%Y%m%d_%H%M%S` # make a time stamp
 	
 	## Set Volume name as variable
 	volume=`isoinfo -d -i /dev/sr1  | grep "^Volume id:" | cut -d ":" -f 2` # gets volume name
-	volume=`echo $volume | tr -d [:punct:] | sed 's/\s/\_/g'` # strips out punct and converts spaces to _	
-	#if volume name is null...
+	volume=`echo $volume | tr -d [:punct:] | sed 's/\s/\_/g'` # strips out punctuation and converts spaces to _	
+	#if volume name is null set it to the timestamp...
         if [[ -z "$volume" ]]; then
           volume="null-volume-$time"
         fi
 
-	time=`date +%Y%m%d_%H%M%S` # make a time stamp
-	
 	path=`realpath $1` # assumes you gave a path as an argument
-	echo $path/$volume.iso # assemble file name for destination .iso, echo for error checking
+	echo "Creating: ${path}/${volume}.iso" # assemble file name for destination .iso, echo for error checking
 	
 			# Display cd Info
 	echo "---------------------CD INFO-----------------------"  
@@ -163,7 +175,7 @@ while [ "s_status" != "+S14" ]; do
 	s_statuspick=`echo $outputpick | grep -oP "\+S\d\d"` # get the S-code from the $autoloader response, $autoloader S10 = drive closed, can't pick, sometimes there is an issue here
 	
 	if [[ $s_statuspick == "+S10" ]]; then
-	  echo "Drive is still closed, not ejecting...EXITING"
+	  echo "Drive is still closed, not ejecting...EXITING SCRIPT"
 	  exit 0	 
 	fi
 		
